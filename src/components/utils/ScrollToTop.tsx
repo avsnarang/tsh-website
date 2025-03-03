1d9babceb8b5a7de66442496bacb5a6c11812ecb
Aditya Veer Singh Narang <avsnarang@tsh.edu.in>
2025-03-03 18:00:37 +0530

    added scroll to top animation for page changes and link clicks

--- src/components/utils/ScrollToTop.tsx
+++ src/components/utils/ScrollToTop.tsx
@@ -5,10 +5,18 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+    const scrollToTop = () => {
+      const c = document.documentElement.scrollTop || document.body.scrollTop;
+      if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo({
+          top: c - c / 8,
+          behavior: 'smooth'
+        });
+      }
+    };
+
+    scrollToTop();
   }, [pathname]);
 
   return null;
